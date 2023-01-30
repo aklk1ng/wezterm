ae82ef4efa4167639574e1432eb5cf5f2a311861
aklk1ng <2628084745@qq.com>
2023-01-30 16:12:23 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -1,48 +1,50 @@
 local wezterm = require 'wezterm'
 
 return {
-  -- Smart tab bar [distraction-free mode]
-  hide_tab_bar_if_only_one_tab = true,
+    -- Smart tab bar [distraction-free mode]
+    hide_tab_bar_if_only_one_tab = true,
 
-  -- Color scheme
-  -- https://wezfurlong.org/wezterm/config/appearance.html
-  --
-  -- color_scheme = "OneDark (base16)",
-  color_scheme = "Dracula",
-  -- color_scheme = "Dracula (Official)",
+    -- Color scheme
+    -- https://wezfurlong.org/wezterm/config/appearance.html
+    --
+    -- color_scheme = "OneDark (base16)",
+    color_scheme = "Dracula",
+    -- color_scheme = "Dracula (Official)",
 
-  scrollback_lines = 3500,
+    scrollback_lines = 3500,
 
-  window_background_opacity = 0.90,
-  window_close_confirmation = "NeverPrompt",
-  window_padding = {
-      left = 0,
-      right = 0,
-      top = 0,
-      bottom = 0,
-  },
-
-
-  -- Font configuration
-  -- https://wezfurlong.org/wezterm/config/fonts.html
-  font = wezterm.font('Cousine Nerd Font'),
-  font_size = 11.5,
-
-  -- Disable ligatures
-  -- https://wezfurlong.org/wezterm/config/font-shaping.html
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-
-  leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 },
-  keys = {
-    {
-      key = '|',
-      mods = 'LEADER|SHIFT',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    window_background_opacity = 0.90,
+    window_close_confirmation = "NeverPrompt",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
     },
-    {
-      key = '-',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+
+
+    -- Font configuration
+    -- https://wezfurlong.org/wezterm/config/fonts.html
+    font = wezterm.font('Cousine Nerd Font'),
+    font_size = 11.5,
+    underline_position = -2,
+    underline_thickness = "150%",
+
+    -- Disable ligatures
+    -- https://wezfurlong.org/wezterm/config/font-shaping.html
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+
+    leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 },
+    keys = {
+        {
+            key = '|',
+            mods = 'LEADER|SHIFT',
+            action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+        },
+        {
+            key = '-',
+            mods = 'LEADER',
+            action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+        },
     },
-  },
 }
